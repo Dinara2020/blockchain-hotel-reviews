b8cdd2e2b561b932cbad56961afb71bf513b6c35
Dinara Khazhipova <d.khazhipova@axioma.lv>
2025-05-25 13:30:03 +0300

    fix autoload

--- src/Http/Services/UserWalletService.php
+++ src/Http/Services/UserWalletService.php
@@ -5,27 +5,36 @@ namespace HotelReviews\Http\Services;
 use HotelReviews\Models\UserWallet;
 use Illuminate\Support\Facades\Auth;
 use kornrunner\Keccak;
-use Elliptic\EC;
+use BitWasp\Bitcoin\Crypto\Random\Random;
+use Mdanter\Ecc\EccFactory;
 
 class UserWalletService
 {
     public function findOrCreateForUser($user): UserWallet
     {
         $wallet = UserWallet::where('user_id', $user->id)->first();
-        if ($wallet) return $wallet;
+        if ($wallet) {
+            return $wallet;
+        }
 
-        $ec = new EC('secp256k1');
-        $key = $ec->genKeyPair();
-        $privateKey = $key->getPrivate('hex');
-        $publicKey = $key->getPublic(false, 'hex');
-        $publicKey = substr($publicKey, 2);
+        $random = new Random();
+        $privateKeyHex = bin2hex($random->bytes(32));
 
-        $address = '0x' . substr(Keccak::hash(hex2bin($publicKey), 256), 24);
+        $adapter = EccFactory::getAdapter();
+        $generator = EccFactory::getSecgCurves()->generator256k1();
+        $privateKey = $generator->createPrivateKey(gmp_init($privateKeyHex, 16));
+        $publicKey = $privateKey->getPublicKey();
+
+        $x = str_pad(gmp_strval($publicKey->getX(), 16), 64, '0', STR_PAD_LEFT);
+        $y = str_pad(gmp_strval($publicKey->getY(), 16), 64, '0', STR_PAD_LEFT);
+        $uncompressedPublicKey = hex2bin('04' . $x . $y);
+
+        $address = '0x' . substr(Keccak::hash($uncompressedPublicKey, 256), 24);
 
         return UserWallet::create([
             'user_id' => $user->id,
             'eth_address' => $address,
-            'eth_private_key' => $privateKey,
+            'eth_private_key' => $privateKeyHex,
         ]);
     }
 }
